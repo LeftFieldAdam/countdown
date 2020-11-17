2a62d09d8e41b0a81445602b58c1efd8cda29708
LeftFieldAdam <69173219+LeftFieldAdam@users.noreply.github.com>
2020-11-17 12:27:54 -0800

    create project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './style/Countdown.css';
+import Countdown from './components/Countdown';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Countdown</h1>
+      </header>
+      <div className="content">
+        <Countdown date='2020-12-24T00:00:00' />,
+      </div>
+    </div>
+  );
+}
+
+export default App;
