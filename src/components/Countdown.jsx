28252dcc8be3752a59131d16dd287da01e0293ee
natatat <natuduwela@gmail.com>
2020-11-24 16:36:28 -0800

    don't allow negative values
    use if instead of while

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -64,8 +64,12 @@ class Countdown extends React.Component {
   }
 
   addLeadingZeros(value) {
+    if (value < 0) {
+      return '00';
+    }
+
     value = String(value);
-    while (value.length < 2) {
+    if (value.length < 2) {
       value = '0' + value;
     }
     return value;
