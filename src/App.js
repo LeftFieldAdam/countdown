bd3e3bb83ca6545ba95bd1aabdfb0e18c629bca9
qbdumont <qbdumont@gmail.com>
2021-08-25 17:01:58 -0700

    setup globals, theme, and cssbaseline implentation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import './style/Countdown.css';
 import Countdown from './components/Countdown';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Countdown</h1>
-      </header>
       <div className="content">
         <Countdown targetDate="2021-12-24T00:00:00" />
       </div>
