83649be62259f543fcaffd33c31881c01392a53b
Mason Aviles <mce.aviles@gmail.com>
2021-09-09 21:30:59 -0700

    add font family to header, update readme

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header py-8">
-        <h1 className="text-6xl">Birthday Countdown</h1>
+        <h1 className="font-sans text-6xl">Birthday Countdown</h1>
       </header>
       <div className="content bg-gray-100 pb-32">
         <Countdown date='2021-12-24T00:00:00' />,
